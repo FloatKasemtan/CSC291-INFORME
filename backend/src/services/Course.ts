f622116799a334a4ce82500adc272d6a7b9920d4
Mystoryme <79404492+Mystoryme@users.noreply.github.com>
2022-06-12 16:02:35 +0700

    feat: add get course endpoints

--- backend/src/services/Course.ts
+++ backend/src/services/Course.ts
@@ -4,9 +4,35 @@ import { genericError, infoResponse } from "./Handler";
 
 const prisma = new PrismaClient();
 
-export const getCourse = async (course_id) => {
+export const getCourse = async (course_id: string) => {
 	try {
-		return infoResponse({}, "found course");
+		const course = await prisma.course.findFirst({
+			where: {
+				id: course_id,
+			},
+			include: {
+				lecturer: {
+					include: {
+						user: {
+							select: {
+								email: true,
+								firstname: true,
+								lastname: true,
+								id: true,
+							},
+						},
+					},
+				},
+			},
+		});
+		const students = await prisma.userClass.findMany({
+			where: {
+				course_id: course_id,
+			},
+			include: { student: { include: { user: true } } },
+		});
+
+		return infoResponse({ course, students }, "found course");
 	} catch (error) {
 		return genericError(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
